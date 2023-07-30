7f964e5d59f3861aca4dd4adf4e35b6424ca0bc0
minhajul-im <minhajul.cpp@gmail.com>
2023-07-30 15:21:26 +0600

    useEffect implementation

--- src/useEffet/UseEffect.jsx
+++ src/useEffet/UseEffect.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+
 const data = `
 useEffect is a built-in hook that allows functional components to perform side effects. Side effects are actions that are executed outside the scope of the component rendering, such as data fetching, subscriptions, or DOM manipulation.
 `;
 const UseEffect = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const date = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(date);
+  }, [time]);
+
   return (
     <div>
+      <h3 style={{ textAlign: "center" }}> {time.toLocaleTimeString()} </h3>
       <h1> What is useEffect?</h1>
       <p> {data} </p>
     </div>
