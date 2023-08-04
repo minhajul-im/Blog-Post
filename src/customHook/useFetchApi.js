5b740c4d8bb2fc03f3ccff520d837eeb57118c01
minhajul-im <minhajul.cpp@gmail.com>
2023-08-04 18:05:33 +0600

    data fetching handle error loading

--- src/customHook/useFetchApi.js
+++ src/customHook/useFetchApi.js
@@ -4,6 +4,32 @@ const useFetchApi = (url, callback) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const fetchApi = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const dataSheet = await res.json();
+
+      if (callback) {
+        setData(callback(dataSheet));
+      } else {
+        setData(dataSheet);
+      }
+
+      setError("");
+      setLoading(false);
+    } catch (e) {
+      setError(e.message);
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    error,
+    loading,
+  };
 };
 
 export default useFetchApi;
