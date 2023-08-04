fb4b6704b0fb18b145fe83b8cf070002a17ab406
minhajul-im <minhajul.cpp@gmail.com>
2023-08-04 21:41:14 +0600

    data show on the ui

--- src/customHook/PostApi.jsx
+++ src/customHook/PostApi.jsx
@@ -5,18 +5,33 @@ const url = `https://jsonplaceholder.typicode.com/posts`;
 const PostApi = () => {
   // custom hook
   const posts = useFetchApi(url, (data) =>
-    data.map((item) => ({
+    data.slice(0, 20).map((item) => ({
       id: item.id,
       title: item.title,
       post: item.body,
     }))
   );
 
-  console.log(posts);
+  // it's a array
+  const myData = posts.data;
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Hello Api Fetch</h1>
+
+      {/* /* check loading or error  */}
+
+      {posts?.loading && <h3>Loading.........</h3>}
+      {posts?.error && <h3> {posts?.error} </h3>}
+
+      {/*  not error or loading  then display data  * */}
+      {myData.map((item, index) => (
+        <div key={item.id}>
+          <h6> {index} </h6>
+          <h4> {item.title} </h4>
+          <p>{item.post} </p>
+        </div>
+      ))}
     </div>
   );
 };
