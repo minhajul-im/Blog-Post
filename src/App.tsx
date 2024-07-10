30786fb8ed7dc5e86e69b1fc597d0f2d90353276
minhajul <dev.minhajul@gmail.com>
2024-07-10 18:10:25 +0600

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR...
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
