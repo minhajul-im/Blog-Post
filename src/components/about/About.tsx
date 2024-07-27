4880d07e5097cbd04810b1e4994c1d0d9b477601
minhajul <dev.minhajul@gmail.com>
2024-07-27 21:46:02 +0600

    added other section just for demo will be updated

---
+++ src/components/about/About.tsx
@@ -0,0 +1,42 @@
+import './About.css';
+
+const About = () => {
+  return (
+    <div id="about">
+      <h1 className="text-2xl font-bold text-center my-6"> About us</h1>
+      <div className="w-full h-[1200px] flex justify-center items-center">
+        <div className="loader">
+          <div className="box box0">
+            <div></div>
+          </div>
+          <div className="box box1">
+            <div></div>
+          </div>
+          <div className="box box2">
+            <div></div>
+          </div>
+          <div className="box box3">
+            <div></div>
+          </div>
+          <div className="box box4">
+            <div></div>
+          </div>
+          <div className="box box5">
+            <div></div>
+          </div>
+          <div className="box box6">
+            <div></div>
+          </div>
+          <div className="box box7">
+            <div></div>
+          </div>
+          <div className="ground">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
