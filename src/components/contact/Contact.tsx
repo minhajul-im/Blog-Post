b4297afc3d3a906660b7b0becbc534bef9eb419c
minhajul <dev.minhajul@gmail.com>
2024-07-28 09:03:48 +0600

    feature footer section slightly done

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,49 +1,11 @@
+import Heading from '../common/Heading';
+
 const Contact = () => {
   return (
-    <div id="contact">
-      <h1 className="text-2xl font-bold text-center my-6"> Contact us</h1>
-      <div className="w-full h-[1200px] flex justify-center items-center">
-        <div className="bg-BG-dark">
-          <p className="text-TX-main">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum,
-            ratione corporis deserunt ipsa quis iste nostrum. Ratione soluta
-            velit beatae repellat iusto enim accusantium quo earum impedit natus
-            praesentium nisi, facilis, voluptatum fugit minus, quidem
-            dignissimos molestiae laboriosam deserunt totam! Provident optio
-            commodi ut officiis incidunt, quas dolore, voluptas nihil,
-            necessitatibus minima cum autem corporis saepe. Iusto accusantium ex
-            eius porro officiis, tenetur dolore ratione inventore architecto
-            sunt minima fugiat id perspiciatis amet voluptatibus harum fugit
-            laboriosam? Odio ut velit quibusdam error consequatur ratione, alias
-            eos expedita nobis fugiat omnis culpa. Voluptatum, ad. Saepe velit,
-            magni possimus explicabo rerum, debitis ipsa ratione illum sunt
-            animi at nostrum veniam fugit excepturi! Veniam repellat optio ullam
-            cum labore reprehenderit magni. Expedita, totam eius quod labore,
-            quasi consequuntur impedit voluptatem cumque, tempore iure
-            voluptatum corporis sint veritatis nulla neque deleniti quibusdam
-            veniam libero unde? Architecto neque quisquam dolorem ipsa
-            repellendus ducimus accusamus aliquam. Eaque maxime id officia culpa
-            dolore quaerat sit alias quia temporibus totam aliquid nisi
-            accusantium, rerum architecto ut iste deserunt eveniet
-            necessitatibus ipsam veritatis? Labore, sit modi commodi reiciendis
-            aperiam eaque quia deleniti. Quia dolorem iste odit in, harum, ab
-            perspiciatis praesentium eveniet earum repellat reprehenderit neque.
-            Quisquam dolorem veniam, perferendis ad adipisci ab mollitia,
-            dignissimos quas suscipit odio debitis veritatis ipsam autem,
-            voluptatibus sequi tenetur? Illo temporibus culpa blanditiis
-            mollitia magni similique a omnis iste corrupti. Laborum, non?
-            Perspiciatis placeat facere minima minus a neque rerum perferendis
-            sed recusandae dolores, doloribus amet dolorum dicta aspernatur!
-            Labore, dolorum dolor qui, soluta tempore libero et quo porro
-            cupiditate eum cum! Expedita nisi mollitia ea ullam nesciunt
-            perspiciatis consequuntur iusto tenetur sit unde non ut animi, eum,
-            labore nulla quos debitis harum dolor qui cumque! Ratione eveniet,
-            quaerat velit id dolorem maiores repellendus minima animi culpa quae
-            nostrum dolor tenetur nisi. Facere?
-          </p>
-        </div>
-      </div>
-    </div>
+    <section id="contact" className="pb-48 mt-16">
+      <Heading>Contact us</Heading>
+      <p>something is coming!!</p>
+    </section>
   );
 };
 
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import './Footer.css';
+import { SOCIALS, SOCIAL_LINKS } from '../../constant/mock-data';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="waves">
+        <div className="wave" id="wave1"></div>
+        <div className="wave" id="wave2"></div>
+        <div className="wave" id="wave3"></div>
+        <div className="wave" id="wave4"></div>
+      </div>
+      <div className="flex gap-4 py-8 items-center">
+        {SOCIALS.map((ICON, idx: number) => (
+          <a className="text-white" key={idx} href={SOCIAL_LINKS[idx]}>
+            <ICON />
+          </a>
+        ))}
+      </div>
+
+      <div className="flex gap-6 items-center text-TX-soft font-semibold pb-4 text-base">
+        <button className="hover:text-TX-dark transition-all duration-100">
+          Terms & Conditions
+        </button>
+        <button className="hover:text-TX-dark transition-all duration-100">
+          Privacy & Policy
+        </button>
+      </div>
+
+      <p className="italic text-sm">&copy;2024 packNjar </p>
+    </footer>
+  );
+};
+
+export default Footer;
